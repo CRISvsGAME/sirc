acf605a7bfc22a00b3706199e45d782f096b9cd3
CRISvsGAME <cris@crisvsgame.com>
2025-05-04 20:00:00 +0000

    SIRC 0.0.19
    Add Core Node Module

---
+++ src/sirc/core/node.py
@@ -0,0 +1,7 @@
+"""
+SIRC Core Node Module.
+
+Defines the Node class used by the SIRC simulation engine. Nodes represent
+logical connection points in the circuit. Multiple Nodes may be connected,
+forming an electrical group that collectively resolves a single LogicValue.
+"""

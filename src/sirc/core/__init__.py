6f86444bc94e7201c5120bd1c43d94c5f4803d1d
CRISvsGAME <cris@crisvsgame.com>
2026-02-27 12:20:13 +0000

    SIRC 1.2.34
    Update Core Module Stub Files

--- src/sirc/core/__init__.py
+++ src/sirc/core/__init__.py
@@ -1,12 +1,17 @@
 """SIRC Core Module."""
 
-from .logic_value import LogicValue
+from .logic_value import LogicValue, ZERO, ONE, X, Z, RESOLVE_TABLE
 from .node import Node, NodeKind
 from .logic_device import LogicDevice, LogicDeviceKind, VDD, GND, Input, Probe, Port
 from .transistor import Transistor, TransistorKind, NMOS, PMOS
 
 __all__ = [
     "LogicValue",
+    "ZERO",
+    "ONE",
+    "X",
+    "Z",
+    "RESOLVE_TABLE",
     "Node",
     "NodeKind",
     "LogicDevice",
--- src/sirc/core/__init__.pyi
+++ src/sirc/core/__init__.pyi
@@ -1,6 +1,6 @@
 from .logic_device import GND as GND, Input as Input, LogicDevice as LogicDevice, LogicDeviceKind as LogicDeviceKind, Port as Port, Probe as Probe, VDD as VDD
-from .logic_value import LogicValue as LogicValue
+from .logic_value import LogicValue as LogicValue, ONE as ONE, RESOLVE_TABLE as RESOLVE_TABLE, X as X, Z as Z, ZERO as ZERO
 from .node import Node as Node, NodeKind as NodeKind
 from .transistor import NMOS as NMOS, PMOS as PMOS, Transistor as Transistor, TransistorKind as TransistorKind
 
-__all__ = ['LogicValue', 'Node', 'NodeKind', 'LogicDevice', 'LogicDeviceKind', 'VDD', 'GND', 'Input', 'Probe', 'Port', 'Transistor', 'TransistorKind', 'NMOS', 'PMOS']
+__all__ = ['LogicValue', 'ZERO', 'ONE', 'X', 'Z', 'RESOLVE_TABLE', 'Node', 'NodeKind', 'LogicDevice', 'LogicDeviceKind', 'VDD', 'GND', 'Input', 'Probe', 'Port', 'Transistor', 'TransistorKind', 'NMOS', 'PMOS']
